e6adb90f2d2602fa630d6a205868215abc711bb3
olfa-dallel-enit <olfa.dallel@enit.utm.tn>
2022-06-21 17:59:55 +0000

    execution

---
+++ execution/launchChains.sh
@@ -0,0 +1,9 @@
+
+
+ignite chain serve -c execution/chains/earth/earth.yml --reset-once
+ignite chain serve -c execution/chains/mars/mars.yml --reset-once
+ignite chain serve -c execution/chains/venus/venus.yml --reset-once
+ignite chain serve -c execution/chains/neptune/neptune.yml --reset-once
+ignite chain serve -c execution/chains/jupiter/jupiter.yml --reset-once
+
+
